4b9d1423ea941ef35eba4d6e255acea860b384a6
thiago-kuhl <thiago.kuhl15@gmail.com>
2020-07-15 00:41:46 -0300

    Initial shop project

---
+++ shop/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/product_detail_screen.dart';
+import 'package:shop/views/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'),
+      home: ProductOverviewScreen(),
+      routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen()},
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: Center(
+        child: Text('Vamos desenvolver uma loja?'),
+      ),
+    );
+  }
+}
